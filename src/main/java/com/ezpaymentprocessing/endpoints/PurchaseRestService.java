8875f061f8bbb584e53c74c0ad7cf095a696439b
northeastcoin <code@northeastcoin.com>
2015-04-08 09:06:43 -0400

    Updates to support decoupled services

--- src/main/java/com/ezpaymentprocessing/endpoints/PurchaseRestService.java
+++ src/main/java/com/ezpaymentprocessing/endpoints/PurchaseRestService.java
@@ -1,12 +1,27 @@
 package com.ezpaymentprocessing.endpoints;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+
+
+
+//import org.apache.http.client.ClientProtocolException;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+
+import javax.ws.rs.core.UriBuilder;
 
 import com.ezpaymentprocessing.model.PurchaseRequest;
 import com.ezpaymentprocessing.model.PurchaseResponse;
@@ -21,6 +36,7 @@ public class PurchaseRestService {
 		
 		int purchaseAmount = Integer.parseInt(amountParam);
 		PurchaseResponse purchaseResponse = PurchaseService.execute(purchaseAmount);
+		
 		return Response.status(200).entity(purchaseResponse).build();
  
 	}
@@ -28,14 +44,71 @@ public class PurchaseRestService {
 	@POST
 	@Produces("application/json")
 	@Consumes("application/json")
-	public Response purchase(PurchaseRequest purchaseRequest) 
+	public Response purchase(PurchaseRequest purchaseRequest, @Context HttpServletRequest request) 
 	{
-		System.out.println("PurchaseRequest: " + purchaseRequest);
+		System.out.println("PurchaseRequest: " + purchaseRequest + " for contextPath: " + request.getContextPath());
+		String contextPath = request.getContextPath();
 		PurchaseResponse purchaseResponse = PurchaseService.execute(purchaseRequest.getAmount());
-		
+		process(contextPath);
 		
 		return Response.status(200).entity(purchaseResponse).build();
 	}
+	
+	private void process(String contextPath)
+	{
+		  try {
+			  	if (contextPath.startsWith("/expaymentprocessing"))
+			  	{
+			  		// local deployment
+			  		contextPath = "/merchantservices";
+			  	}
+			  	else
+			  	{
+			  		contextPath = "http://merchantservices-egetchel.rhcloud.com";
+			  	}
+			  			
+				ClientRequest request = new ClientRequest(
+						"http://localhost:8080/merchantservices/rest/processPromotion/10");
+				request.accept("application/json");
+				ClientResponse<String> response = request.get(String.class);
+		 
+				if (response.getStatus() != 200) {
+					throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatus());
+				}
+		 
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+					new ByteArrayInputStream(response.getEntity().getBytes())));
+		 
+				String output;
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					System.out.println(output);
+				}
+		 
+			  } 
+		  catch (Exception E)
+		  {
+			  E.printStackTrace();
+		  }
+		 
+		  /*
+		  catch (ClientProtocolException e) {
+		 
+				e.printStackTrace();
+		 
+			  } catch (IOException e) {
+		 
+				e.printStackTrace();
+		 
+			  } catch (Exception e) {
+		 
+				e.printStackTrace();
+		 
+			  }
+*/		 
+	}
+
 	
 
 
